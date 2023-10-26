6744cccb8d59578cbca45611ac8584025895f9c9
luis011954 <138296604+luis011954@users.noreply.github.com>
2023-10-26 17:53:26 -0600

    Add files via upload

---
+++ EVA2_11_CADENAS/src/eva2_11_cadenas/EVA2_11_CADENAS.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_11_cadenas;
+
+/**
+ *
+ * @author clitx
+ */
+public class EVA2_11_CADENAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String cade = "HOLA";
+        
+           
+        if (cade.equals("HOLAX"))
+            System.out.println("LAS CADENAS SON IGUALES");
+        else
+            System.out.println("LAS CADENAS SON DIFERENTES");
+    }
+    
+}
