1ede524f33c63b27588b6e30ca440ac2ac427069
luis011954 <138296604+luis011954@users.noreply.github.com>
2023-11-01 17:51:58 -0600

    Add files via upload

---
+++ EVA2_17_WHILE/src/eva2_17_while/EVA2_17_WHILE.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_17_while;
+
+/**
+ *
+ * @author clitx
+ */
+public class EVA2_17_WHILE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //EL CICLO WHILE SE EJECUTA CERO A N VECES
+        
+        int i = 10;
+        i = 0;
+        while(i < 10){
+            System.out.println("LUIS!!");
+            i++;
+        }
+    }
+    
+}
